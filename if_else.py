2344d4297aa92f8af8a6e0eff482b64cf691a66a
George Barrett <georgeaustinbarrett@gmail.com>
2022-11-24 15:46:47 +0000

    if_else.py complete with comments

---
+++ if_else.py
@@ -0,0 +1,28 @@
+# variables storing numbers
+people = 30
+cars = 40
+trucks = 15
+
+if cars > people:
+    print('We better take the cars.') # because there are more cars
+
+elif cars < people:
+    print('Cars aren\'t going to cut it.') # because there are too many people
+
+else:
+    print('We can\'t decide.') # the final return
+
+if trucks > cars:
+    print('TOO MANY TRUCKS.')
+
+elif trucks < cars:
+    print('We could use the trucks.')
+
+else:
+    print('We still can\'t decide.')
+
+if people > trucks:
+    print('It\'s truck time.') # you can get loads of people in trucks
+
+else:
+    print('Let\'s just stay in.') # the final return
